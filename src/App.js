7d6947105db9e61de4f2a6807467b2f294bb8c0c
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-05-12 08:18:10 -0300

    testing react-youtube

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
-import {Link,   Route,  BrowserRouter as Router, Switch} from 'react-router-dom';
+import YouTube from 'react-youtube';
+// import {Link,   Route,  BrowserRouter as Router, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './css/pure-min.css';
 import './css/pricing.css'
 
 class App extends Component {
   render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playersVars: {
+        autoplay: 1
+      }
+    };
     return (
-      <Router>
-        
-      </Router>
+      // <Router>
+      //
+      // </Router>
+      <div>
+        <YouTube
+          videoId="9jK-NcRmVcw"
+          opts={opts}
+          onReady={this._onReady}
+        />
+      </div>
     );
+  }
+
+  _onReady(event){
+    event.target.pauseVideo();
   }
 }
 
